30bbaabd0dc891c02b18fe1e8a2215beb00bdc6e
Ian <A01663727@tec.mx>
2025-11-02 15:18:52 -0600

    Corregí el archivo figuras.csv, modifique el archivo figurasPrincipal.py, cree el archivo funcionesFiguras.py para calcular el área de las figuras

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from funcionesFiguras import triangulo, rectangulo, circulo
 
 dataFile = pd.read_csv("figuras.csv")
 
@@ -8,4 +9,13 @@ areas = []
 perimetros = []
 
 for index, row in dataFile.iterrows():
-	print(f"Fila {index}: FIGURA={row['FIGURA']}, Medida1={row['MEDIDA1']}, Medida2={row['MEDIDA2']}")
+    if row['FIGURA'] == 't':
+        area = triangulo(row['MEDIDA1'], row['MEDIDA2'])
+        print(area)
+    if row['FIGURA'] == 'c':
+        area = circulo(row['MEDIDA1'])
+        print(area)
+    if row['FIGURA'] == 'r':
+        area = rectangulo(row['MEDIDA1'], row['MEDIDA2'])
+        print(area)
+
